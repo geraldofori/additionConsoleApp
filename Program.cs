86b28b7ec5036509d605300d6aba230cfaa3b512
Gerald Ofori <oforige@gmail.com>
2023-09-07 13:28:55 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+class Program {
+    [DllImport("libadd.so")]
+    public static extern int add(int a, int b);
+
+    static void Main() {
+        int result = add(8, 8);
+        Console.WriteLine($"Result: {result}");
+    }
+}
